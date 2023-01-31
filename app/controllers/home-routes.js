b1896d9d7fc17dc84cab202d2fee46677357163c
masonMO25 <masonneale@gmail.com>
2023-01-30 23:14:16 -0600

    fixed speeling error

--- app/controllers/home-routes.js
+++ app/controllers/home-routes.js
@@ -77,15 +77,7 @@ router.get("/post/:id", (req, res) => {
     .then((dbPostData) => {
       if (!dbPostData) {
         res.status(404).json({ message: "No post found with this id" });
-        return;
       }
-
-      const post = dbPostData.get({ plain: true });
-
-      res.render("single-post", {
-        post,
-        loggedIn: req.session.loggedIn,
-      });
     })
     .catch((err) => {
       console.log(err);
